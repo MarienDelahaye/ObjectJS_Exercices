d79bd58402ec732ba845e488f6151330440bda6f
Delahaye Marien <delahaye.marien@gmail.com>
2017-08-21 11:16:12 +0200

    Added exercice2.js with a few code lines

---
+++ exercice2.js
@@ -0,0 +1,16 @@
+// Variables //
+
+var character = new object();
+character.name = "Warrior Pepe";
+character.attack = "78";
+character.defense = "65";
+character.health = "236";
+
+
+// Functions //
+
+
+
+
+// Script Part //
+
