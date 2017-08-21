726a12161555b111f0cc65b7210838c131e985a2
Delahaye Marien <delahaye.marien@gmail.com>
2017-08-21 09:41:30 +0200

    First Commit

---
+++ exercice1.js
@@ -0,0 +1,16 @@
+// Variables //
+
+var user = new Object();
+user.Name = "prénom";
+user.FamilyName = "Nom de famille";
+user.Country = "France";
+user.Age = "21";
+user.PaymentMethod = "VISA, MASTERCARD...";
+
+// Functions //
+
+
+
+// Script Part //
+alert (user.Name);
+alert (user.PaymentMethod)
