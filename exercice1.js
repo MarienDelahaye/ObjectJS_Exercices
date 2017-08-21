b4ff61414741b3dc3a01d94aad0030d6f279c2f7
Delahaye Marien <delahaye.marien@gmail.com>
2017-08-21 10:41:25 +0200

    Added more code in exercice1.js

--- exercice1.js
+++ exercice1.js
@@ -1,20 +1,23 @@
 // Variables //
 
 var user = new Object();
-user.Name = "prénom";
-user.FamilyName = "Nom de famille";
+user.Name = "Marien";
+user.FamilyName = "DELAHAYE";
 user.Country = "France";
 user.Age = "21";
 user.PaymentMethod = "Visa, MasterCard...";
 
 var userAuth = new Object();
 userAuth.AllowedCountry = "France, Germany, Netherlands, Finland";
-userAuth.SupportedPaymentMethod = "Visa, MasterCard, PayPal";
-
+userAuth.SupportedPaymentMethod = "Visa, MasterCard and PayPal are supported to take order";
+userAuth.MinimumAge = "20 years old is required to enter";
+userAuth.YourCountry = "Your country is France, welcome."
 // Functions //
 
 
 
 // Script Part //
 alert (user.Name);
-alert (user.PaymentMethod)
+alert (user.PaymentMethod);
+alert (userAuth.MinimumAge);
+alert (userAuth.YourCountry);
