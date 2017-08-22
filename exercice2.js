66b103851144a35cd9435dba59e28e5c57417c7c
Delahaye Marien <delahaye.marien@gmail.com>
2017-08-22 11:26:28 +0200

    Added function in exercice2.js & added one button in index2.html

--- exercice2.js
+++ exercice2.js
@@ -20,14 +20,31 @@ var opponent = {
 
 // Functions //
 function getCharacterinfos(){
-    alert("Name: " + character.name)
-    alert("Current level: " + character.level)
+    alert("Name: " + character.name);
+    alert("Current health: " + character.health);
+    alert("Current level: " + character.level);
+    alert("Current attack: " + character.attack);
+    alert("Current defense: " + character.defense);
+
+
 
 }
 function getOpponentinfos(){
-    alert ("Name: " + opponent.name)
-    alert("Current level: " + opponent.level)
+    alert("Name: " + opponent.name);
+    alert("Current health: " + opponent.health);
+    alert("Current level: " + opponent.level);
+    alert("Current attack: " + opponent.attack);
+    alert("Current defense:" + opponent.defense);
 }
-
+function attackTurn(){
+    alert(character.name + " will attack " + opponent.name);
+    alert(opponent.name + " got attacked he lost 88 health points");
+    opponent.health = opponent.health -88;
+    alert("Now " + opponent.name + " have " + opponent.health + " health");
+    alert(opponent.name + " will attack " + character.name);
+    alert(character.name + " got attacked he lost 41 health points")
+    character.health = character.health -41;
+    alert("Now" + character.name + " have " + character.health + " health");
+}
 
 // Script Part //
