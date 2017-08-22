8def0dff0da1497bcb29ff11bf6c1178ee19cd5f
Delahaye Marien <delahaye.marien@gmail.com>
2017-08-22 09:17:41 +0200

    Edited exercice2.js and index2.html

--- exercice2.js
+++ exercice2.js
@@ -1,23 +1,19 @@
 // Variables //
 
 var character = {
-    name: "Warrior Pepe",
-    health: "260",
-    attack: "148",
-    defense: "89",
-    description: function (){
-        var description = this.name + " a " + this.health + " Point de vie, " +
-        this.attack + " en attaque et " + this.defense + " Point de défense";
-
-        return description;
-    }
+    name: "A dankest warrior",
+    health: 260,
+    attack: 148,
+    defense: 89,
 }
 
 
 // Functions //
+function getCharacter(){
+    alert(character.name)
 
+}
 
 
 
 // Script Part //
-alert(character.description())
