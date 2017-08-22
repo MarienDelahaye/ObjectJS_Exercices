10a6018de4709d1837b09fc159099acc99ae3199
Delahaye Marien <delahaye.marien@gmail.com>
2017-08-22 09:59:22 +0200

    Adding content in exercice2.js and index2.html

--- exercice2.js
+++ exercice2.js
@@ -5,15 +5,29 @@ var character = {
     health: 260,
     attack: 148,
     defense: 89,
+    level: 1,
 }
+var opponent = {
+    name: "Magnum Dong Woody",
+    health: 420,
+    attack: 130,
+    defense: 60,
+    level: 1,
+}
+
+
 
 
 // Functions //
-function getCharacter(){
-    alert(character.name)
+function getCharacterinfos(){
+    alert("Name: " + character.name)
+    alert("Current level: " + character.level)
 
 }
-
+function getOpponentinfos(){
+    alert ("Name: " + opponent.name)
+    alert("Current level: " + opponent.level)
+}
 
 
 // Script Part //
